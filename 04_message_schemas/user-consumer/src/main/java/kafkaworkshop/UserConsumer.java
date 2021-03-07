17625aaac38f89dabf5d091b0f42b7f023e941e7
André <p0gram3r@gmail.com>
2021-03-07 21:10:52 +0100

    04_message_schemas

---
+++ 04_message_schemas/user-consumer/src/main/java/kafkaworkshop/UserConsumer.java
@@ -0,0 +1,12 @@
+package kafkaworkshop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserConsumer {
+    private static final Logger log = LoggerFactory.getLogger(UserConsumer.class);
+
+    public static void main(String[] args) {
+        log.info("hello world");
+    }
+}
